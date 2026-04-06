18b644f90bf2e52545696414fe459d895b665946
Yudisssss <gabrielyudi06@gmail.com>
2026-04-06 15:47:39 -0300

    feat: add home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { UsersComponent } from './features/users/users.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-    { path: '**', redirectTo: 'home'},
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'users', component: UsersComponent }
+    { path: 'users', component: UsersComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', redirectTo: 'home'}
 ];
