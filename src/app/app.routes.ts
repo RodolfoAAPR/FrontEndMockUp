6ec9e2562159d5fad4d44c15051f16c5fe32a099
Rodolfo <rodolfo@bitapps.com.br>
2026-05-04 14:47:55 -0300

    feat(routes): add users route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { UsersComponent } from './features/users/users.component';
 
 export const routes: Routes = [
-    { path: '**', redirectTo: 'home'},
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'users', component: UsersComponent },
+    { path: '', redirectTo: '/users', pathMatch: 'full' },
+    { path: '**', redirectTo: 'users' },
 ];
